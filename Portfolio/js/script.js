c2c6d03a0f6e292e649c45782d64024859279a93
deepak-raj2002 <107445481+deepak-raj2002@users.noreply.github.com>
2022-11-09 19:03:30 +0530

    Add files via upload

---
+++ Portfolio/js/script.js
@@ -0,0 +1,8 @@
+// typing animation..........
+
+var typed=new Typed(".typing",{
+    strings:["","Web Developer","Web Designer","Competitive Coder"],
+    typespeed:40,
+    backSpeed:0,
+    loop:true
+})
